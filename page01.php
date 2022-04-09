dccf4d13e20cd56a52c42b40f327c81a787abde2
Miguel <103295886+migguel01@users.noreply.github.com>
2022-04-08 23:34:28 -0300

    2.ª aula de PHP com Banco de Dados

--- page01.php
+++ page01.php
@@ -10,7 +10,7 @@ $carro = "Fusca";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>2ª. aula de PHP com Banco de Dados</title>
+    <title>2.ª aula de PHP com Banco de Dados</title>
 </head>
 <body>
 
