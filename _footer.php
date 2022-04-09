fb264243391a7ede7a64f26f02311786f7e383a3
Miguel <103295886+migguel01@users.noreply.github.com>
2022-04-08 23:32:41 -0300

    3.ª aula de PHP com Banco de Dados

---
+++ _footer.php
@@ -0,0 +1,9 @@
+</main>
+        <footer>
+            <a href="/" title="Página Inicial"><i class="fa-solid fa-house-chimney"></i></a>
+            <div title="Todos os direitos reservados.">&copy; Copyright 2022 Vitugo.</div>
+            <a href="#top" title="Voltar ao topo da página"><i class="fa-solid fa-circle-up"></i></a>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ modelo.php
@@ -0,0 +1,20 @@
+<?php
+
+// Inclui o cabeçalho da página
+require('_header.php');
+?>
+            <section>
+                <h2>Título da página</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui eveniet soluta magnam nulla omnis fugit ex, fuga accusamus perspiciatis hic. Aspernatur ut quo officia veniam animi maiores ipsam saepe recusandae.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sit eius quas rem eligendi dolorum quod consectetur ullam, sint fugit vero adipisci placeat reprehenderit itaque dicta facere repellat quae. Magni.</p>
+            </section>
+            <aside>
+                <h3>Lateral</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </aside>
+
+<?php
+
+//Inclui o rodapé da página
+require('_footer.php');
+?>
