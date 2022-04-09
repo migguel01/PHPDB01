9b5307d2281f93c6a8348bfe16d48650c8bd84ed
Miguel <103295886+migguel01@users.noreply.github.com>
2022-04-08 23:03:26 -0300

    2ª. aula de PHP com Banco de Dados

---
+++ page01.php
@@ -0,0 +1,25 @@
+<?php
+
+// Cria uma variável
+$carro = "Fusca";
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2ª. aula de PHP com Banco de Dados</title>
+</head>
+<body>
+
+<?php
+    echo $carro 
+?>
+ 
+<?php // Isso aqui é uma imagem aleatória ?>
+ <!-- <img src="https://picsum.photos/200/300" alt="Imagem aleatória"> -->
+
+</body>
+</html>
